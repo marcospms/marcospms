27384e15fc5ba3a45f5bebf340ecfec15e297cd9
marcospms <marcospms@gmail.com>
2021-04-27 19:43:33 -0300

    commit das requisicoes funcional

--- Prova V1 Server api/app.js
+++ Prova V1 Server api/app.js
@@ -1,33 +1,44 @@
 var express = require("express");
 var app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
-app.get("/nomes", (req, res, next) =>
-{
-    res.json(["Solicitação abertura de página"]);
+app.get("/lancamentos", (req, res) => {
+    console.log("Requisição Get realizada com Sucesso");
+    res.status(200).json(["Paulo Osvaldo da Silva", false, 5, 84.00]);
 });
-app.post('/nomes', (req, res) =>
-{
-    console.log("Chegou no Post - Lançamento de Contas - Inclusão de Registro...");
-    res.status(200).json(["Paulo Osvaldo da Silva","Conta ativa","Compra parcelada","5x","84,00"]);
-    res.status(200).json(["Samanta Oliveira Ferreira","Conta ativa","Compra parcelada","10x","126,80"])
-});
-app.put('/nomes', (req, res) =>
-{
-    console.log("Chegou no Put - Consulta de Lançamentos e Atualização de Registro...");
-    res.status(200).json(["Paulo Osvaldo da Silva","Conta ativa","Compra parcelada","10x","56,00"]);
-    res.status(200).json(["Samanta Oliveira Ferreira Santos","Conta ativa","Compra parcelada","10x","126,80"]);
+app.post("/lancamento", (req, res) => {
+    console.log("Requisição Post realizada com Sucesso");
+    console.log(req.body);
+    console.log("Nome: " + req.body.nome);
+    console.log("Ativo: " + req.body.ativo);
+    console.log("Parcelas: " + req.body.parcelas);
+    console.log("Valor: " + req.body.valor);
 
-});
-app.delete('/nomes/:id',(req, res) =>
-{
-    console.log("Chegou no Delete - ");
-    res.status(200).send('Registro Deletado');
+
+    res.send("Mensagem Enviada - Post");
 });
 
+app.put("/lancamento/:id", (req, res) => {
+    console.log("Requisição Put realizada com Sucesso");
+    console.log(req.body);
+    console.log("id: " + req.params.id)
+    console.log("Nome: " + req.body.nome);
+    console.log("Ativo: " + req.body.ativo);
+    console.log("Parcelas: " + req.body.parcelas);
+    console.log("Valor: " + req.body.valor);
 
 
-app.listen(4000, () =>
-{
+    res.send("Mensagem Enviada - Put");
+});
+app.delete("/lancamento/:id", (req, res) => {
+    console.log("Requisição Delete realizada com Sucesso");
+    console.log("O Lançamento com o código " + req.params.id + " Será Excluido");
+    res.status(200).send("Lançamento Excluido");
+});
+
+app.listen(4000, () => {
     console.log("Servidor rodando na porta 4000");
 });
 
